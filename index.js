ab96d0f87fa9610a1f24ffe771fc59de27b66f06
GodwinNgbede <godwinslash75@gmail.com>
2025-09-24 22:28:50 +0100

    Added the new  arrival section and its functionality

--- index.js
+++ index.js
@@ -8,12 +8,32 @@ function scrollHeader() {
   }
   window.addEventListener("scroll", scrollHeader);
 }
-// SWIPER INITIALIZER FN
+// HOME SWIPER INITIALIZER FN
 var homeSwiper = new Swiper(".home-swiper", {
   spaceBetween: 30,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+});
+
+// NEW SWIPER
+var newSwiper = new Swiper(".new-swiper", {
+  centeredSlide: true,
+
+  loop: true,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // for phones
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
   },
 });
